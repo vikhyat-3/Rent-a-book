0f4a42f5f86ade24c957a27717caf4b88da27dd5
unknown <vvk212001@gmail.com>
2023-09-27 23:24:56 +0530

    Added book and author entities

---
+++ src/main/java/com/spring/RentABook/entity/Author.java
@@ -0,0 +1,76 @@
+package com.spring.RentABook.entity;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.validation.annotation.Validated;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.AccessMode;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Validated
+@Document(collection = "Author")
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue
+    @Schema(accessMode = AccessMode.READ_ONLY)
+    private String id;
+
+    @NotBlank(message = "Please enter author name")
+    @NotNull(message = "Please enter author name")
+    @Size(min = 2, max = 100)
+    @Length(min = 2, max = 100)
+    private String name;
+
+    private Gender gender;
+
+    private List<Book> books;
+
+    public Author(String id,
+            @NotBlank(message = "Please enter author name") @NotNull(message = "Please enter author name") @Size(min = 2, max = 100) @Length(min = 2, max = 100) String name,
+            Gender gender, List<Book> books) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.books = books;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+
+}
